85c9c6ffd2c3a698f3cb2594737625567f849ebc
Phoebe <phoebe.aqwu@gmail.com>
2020-10-05 19:51:55 -0700

    Added getters and setters

---
+++ src/main/model/FavouritesList.java
@@ -0,0 +1,14 @@
+package model;
+
+import java.util.List;
+
+public class FavouritesList {
+
+    public String name;
+    private List<CollectionCenter> centers;
+
+    public FavouritesList() {
+        //stub
+    }
+
+}
